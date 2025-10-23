b4d1ec267b35a709ced008cf54243c2d2aaf6a8a
Joe-Daniel29 <very.joe.daniel@gmail.com>
2025-10-23 18:50:02 +0530

    commit2

--- server/server.js
+++ server/server.js
@@ -25,7 +25,8 @@ app.use(cors({
   origin: [
     'http://localhost:3000',
     'http://localhost:5173',
-    'https://trello-kanban-joe.vercel.app'
+    'https://trello-kanban-joe.vercel.app',
+    'https://trello-kanban-backend-joe.vercel.app'
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
